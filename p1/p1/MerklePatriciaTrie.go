610a8476986a3594da4f72b3141f718dfcaab251
CharlesGe129 <char1209ge@gmail.com>
2019-02-10 19:03:30 -0800

    Test cases

---
+++ p1/p1/MerklePatriciaTrie.go
@@ -0,0 +1,156 @@
+package p1
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/sha3"
+	"reflect"
+	"strings"
+)
+
+type Flag_value struct {
+	encoded_prefix []uint8
+	value string
+}
+
+type Node struct {
+	node_type int // 0: Null, 1: Branch, 2: Ext or Leaf
+	branch_value [17]string
+	flag_value Flag_value
+}
+
+type MerklePatriciaTrie struct {
+	db map[string]Node
+	root string
+}
+
+func (mpt *MerklePatriciaTrie) Get(key string) (string, error) {
+	// TODO
+	return "", errors.New("path_not_found")
+}
+
+func (mpt *MerklePatriciaTrie) Insert(key string, new_value string) {
+	// TODO
+}
+
+func (mpt *MerklePatriciaTrie) Delete(key string) error {
+	// TODO
+	return errors.New("path_not_found")
+}
+
+func compact_encode(hex_array []uint8) []uint8 {
+	// TODO
+	return []uint8{}
+}
+
+// If Leaf, ignore 16 at the end
+func compact_decode(encoded_arr []uint8) []uint8 {
+	// TODO
+	return []uint8{}
+}
+
+func test_compact_encode() {
+	fmt.Println(reflect.DeepEqual(compact_decode(compact_encode([]uint8{1, 2, 3, 4, 5})), []uint8{1, 2, 3, 4, 5}))
+	fmt.Println(reflect.DeepEqual(compact_decode(compact_encode([]uint8{0, 1, 2, 3, 4, 5})), []uint8{0, 1, 2, 3, 4, 5}))
+	fmt.Println(reflect.DeepEqual(compact_decode(compact_encode([]uint8{0, 15, 1, 12, 11, 8, 16})), []uint8{0, 15, 1, 12, 11, 8}))
+	fmt.Println(reflect.DeepEqual(compact_decode(compact_encode([]uint8{15, 1, 12, 11, 8, 16})), []uint8{15, 1, 12, 11, 8}))
+}
+
+func (node *Node) hash_node() string {
+	var str string
+	switch node.node_type {
+	case 0:
+		str = ""
+	case 1:
+		str = "branch_"
+		for _, v := range node.branch_value {
+			str += v
+		}
+	case 2:
+		str = node.flag_value.value
+	}
+
+	sum := sha3.Sum256([]byte(str))
+	return "HashStart_" + hex.EncodeToString(sum[:]) + "_HashEnd"
+}
+
+func (node *Node) String() string {
+	str := "empty string"
+	switch node.node_type {
+	case 0:
+		str = "[Null Node]"
+	case 1:
+		str = "Branch["
+		for i, v := range node.branch_value[:16] {
+			str += fmt.Sprintf("%d=\"%s\", ", i, v)
+		}
+		str += fmt.Sprintf("value=%s]", node.branch_value[16])
+	case 2:
+		encoded_prefix := node.flag_value.encoded_prefix
+		node_name := "Leaf"
+		if is_ext_node(encoded_prefix) {
+			node_name = "Ext"
+		}
+		ori_prefix := strings.Replace(fmt.Sprint(compact_decode(encoded_prefix)), " ", ", ", -1)
+		str = fmt.Sprintf("%s<%v, value=\"%s\">", node_name, ori_prefix, node.flag_value.value)
+	}
+	return str
+}
+
+func node_to_string(node Node) string {
+	return node.String()
+}
+
+func (mpt *MerklePatriciaTrie) Initial() {
+	mpt.db = make(map[string]Node)
+}
+
+func is_ext_node(encoded_arr []uint8) bool {
+	return encoded_arr[0] / 16 < 2
+}
+
+func TestCompact() {
+	test_compact_encode()
+}
+
+func (mpt *MerklePatriciaTrie) String() string {
+	content := fmt.Sprintf("ROOT=%s\n", mpt.root)
+	for hash := range mpt.db {
+		content += fmt.Sprintf("%s: %s\n", hash, node_to_string(mpt.db[hash]))
+	}
+	return content
+}
+
+func (mpt *MerklePatriciaTrie) Order_nodes() string {
+	raw_content := mpt.String()
+	content := strings.Split(raw_content, "\n")
+	root_hash := strings.Split(strings.Split(content[0], "HashStart")[1], "HashEnd")[0]
+	queue := []string{root_hash}
+	i := -1
+	rs := ""
+	cur_hash := ""
+	for len(queue) != 0 {
+		last_index := len(queue) - 1
+		cur_hash, queue = queue[last_index], queue[:last_index]
+		i += 1
+		line := ""
+		for _, each := range content {
+			if strings.HasPrefix(each, "HashStart" + cur_hash + "HashEnd") {
+				line = strings.Split(each, "HashEnd: ")[1]
+				rs += each + "\n"
+				rs = strings.Replace(rs, "HashStart" + cur_hash + "HashEnd", fmt.Sprintf("Hash%v", i),  -1)
+			}
+		}
+		temp2 := strings.Split(line, "HashStart")
+		flag := true
+		for _, each := range temp2 {
+			if flag {
+				flag = false
+				continue
+			}
+			queue = append(queue, strings.Split(each, "HashEnd")[0])
+		}
+	}
+	return rs
+}
